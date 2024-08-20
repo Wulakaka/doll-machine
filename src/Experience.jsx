6476102cfd065cdbc7951c4e1bd2677cebb19e31
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-20 16:08:55 +0800

    feat: 调整光照参数；删除无用属性

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,30 @@
 import { Perf } from 'r3f-perf'
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import {
+  OrbitControls,
+  CameraControls,
+  useHelper,
+  Helper,
+} from '@react-three/drei'
 import { Machine } from './Machine.jsx'
+import { useEffect, useRef } from 'react'
+import { DirectionalLightHelper, CameraHelper } from 'three'
+import { useThree } from '@react-three/fiber'
 
 export default function Experience() {
-  const model = useGLTF('./model.glb')
+  const directionalLight = useRef()
+
+  const { scene } = useThree()
+
+  // DirectionalLightHelper
+  // useHelper(directionalLight, DirectionalLightHelper, 1, 'red')
+
+  // useEffect(() => {
+  //   if (directionalLight.current) {
+  //     const helper = new CameraHelper(directionalLight.current.shadow.camera)
+  //     scene.add(helper)
+  //   }
+  // }, [directionalLight])
+
   return (
     <>
       <Perf position="top-left" />
@@ -12,7 +33,19 @@ export default function Experience() {
 
       <ambientLight intensity={0.5} color={'white'}></ambientLight>
 
-      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+      <directionalLight
+        ref={directionalLight}
+        castShadow
+        position={[-1, 3, 2]}
+        intensity={4.5}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={5}
+        shadow-camera-left={-1}
+        shadow-camera-right={1}
+        shadow-camera-top={1.5}
+        shadow-camera-bottom={-0.5}
+      />
 
       <group>
         <Machine />
